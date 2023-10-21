3ad891813bee4b55870a906625ae422bb38f6dfc
chxxah <chxxxah@gmail.com>
2023-10-22 00:50:52 +0900

    [2023-10-21]

---
+++ workspace/oct21/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author chxxah
+ *
+ */
+module oct21 {
+}
---
+++ workspace/oct21/src/oct21/TestController.java
@@ -0,0 +1,8 @@
+package oct21;
+
+public class TestController {
+	public static void main(String[] args) {
+		System.out.println("1");
+	}
+
+}
